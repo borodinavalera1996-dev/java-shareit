2af8c47be01cbc509ff8ac7dd077773f2583ba66
valery <vborodina@emdev.ru>
2026-08-25 19:41:35 +0300

    fix tests

--- src/main/java/ru/practicum/shareit/booking/dto/BookingInputDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingInputDto.java
@@ -1,12 +1,17 @@
 package ru.practicum.shareit.booking.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class BookingInputDto {
+    @NotNull
     private Long itemId;
+    @NotNull
+    @FutureOrPresent
     private LocalDateTime start;
     private LocalDateTime end;
 }
